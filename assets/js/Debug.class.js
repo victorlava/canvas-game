056721a166eebc0ea343471cc3bfb0b8d14d9e6e
Victor Lava <hello@victorlava.com>
2018-03-03 11:57:54 +0200

    added comments to Debug.class.js

--- assets/js/Debug.class.js
+++ assets/js/Debug.class.js
@@ -1,16 +1,31 @@
+/**
+ * Calls a debug class, which is responsible for debugging elements on canvas
+ * @class debug
+ */
 class Debug {
 
     constructor() {
 
     }
-    
+
+    /**
+     * Used for displaying coordinates of the object
+     * @function coordinates
+     * @param {object} object Can anything that has dimensions property
+     */
     coordinates(object) {
         var padding = 10,
             location = object.dimensions,
             text = 'x: ' + this.round(location.x) + ', y: ' + this.round(location.y);
 
         canvas.text(location.x, location.y - padding, text);
-    }
+
+    /**
+     * Funtionc that rounds number up with the desired precision.
+     * @function round
+     * @param {float | integer} number Number to rounds
+     * @param {integer} precision How much precision to use when rounding up
+     */
     round(number, precision = 0) {
       var factor = Math.pow(10, precision);
       return Math.round(number * factor) / factor;
